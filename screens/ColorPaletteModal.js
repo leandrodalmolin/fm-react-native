bb1bce7e190b696d1d3b5df9bfaaec7365ad6ab0
Leandro Dal Molin <1050873+leandrodalmolin@users.noreply.github.com>
2023-04-28 11:40:04 +0100

    Add input and submit button to the modal window for future palette registration

--- screens/ColorPaletteModal.js
+++ screens/ColorPaletteModal.js
@@ -1,12 +1,65 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useCallback, useState } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
 
 const ColorPaletteModal = () => {
+  const [name, setName] = useState('');
+
+  const handleSubmit = useCallback(() => {
+    if (!name) {
+      Alert.alert('Please enter a palette name');
+    }
+  }, [name]);
+
   return (
-    <View>
-      <Text>Hello, world!</Text>
+    <View style={styles.container}>
+      <Text style={styles.name}>Name of the palette</Text>
+      <TextInput
+        style={styles.input}
+        value={name}
+        onChangeText={setName}
+        placeholder="Palette name"
+      />
+      <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+        <Text style={styles.buttonText}>Submit</Text>
+      </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: 'white',
+  },
+  name: {
+    marginBottom: 10,
+  },
+  input: {
+    borderColor: 'grey',
+    borderWidth: 1,
+    marginBottom: 50,
+    padding: 10,
+    borderRadius: 5,
+  },
+  button: {
+    height: 40,
+    backgroundColor: 'teal',
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
 
 export default ColorPaletteModal;
